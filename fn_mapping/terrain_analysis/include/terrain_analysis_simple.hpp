cf04e1805409e408bd976cbb91e1efb91cef43f6
huigg <wangjinghui@sjtu.edu.cn>
2025-08-21 18:31:23 +0800

    build: 配置pluginlib环境

---
+++ fn_mapping/terrain_analysis/include/terrain_analysis_simple.hpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2025.
+// IWIN-FINS Lab, Shanghai Jiao Tong University, Shanghai, China.
+// All rights reserved.
+
+#pragma once
+
+#include "terrain_analysis_base.hpp"
+
+namespace finenav_2d {
+
+class TerrainAnalysisSimple : public TerrainAnalysisBase {
+    TerrainAnalysisSimple(MapInterface* map_interface) : TerrainAnalysisBase(map_interface) {}
+
+
+};
+
+} // nanamespace finenav_2d
---
+++ fn_mapping/terrain_analysis/src/terrain_analysis_simple.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2025. 
+// IWIN-FINS Lab, Shanghai Jiao Tong University, Shanghai, China.
+// All rights reserved.
+
+#include "terrain_analysis_simple.hpp"
+
+
+namespace finenav_2d {
+
+
+
+} // namespace finenav_2d
+
+#include <pluginlib/class_list_macros.hpp>
+PLUGINLIB_EXPORT_CLASS(finenav_2d::TerrainAnalysisSimple, finenav_2d::TerrainAnalysisBase)
