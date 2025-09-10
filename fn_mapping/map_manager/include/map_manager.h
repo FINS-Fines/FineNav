978de14f1746c11db11ee25a3d26990523bf901a
H1llo <minhangwcy@sjtu.edu.cn>
2025-09-10 11:33:23 +0800

    feat: 增加Octomap可视化

--- fn_mapping/map_manager/include/map_manager.h
+++ fn_mapping/map_manager/include/map_manager.h
@@ -73,6 +73,7 @@ private:
 
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr local_map_pub_;    // 发布局部地图的点云消息
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr ground_pub_;      // 发布分析后的ground信息
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr octomap_pub_;      // 发布分析后的ground信息
     rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr localcost_map_pub_; // 发布局部代价地图
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr test_pub_; // 发布局部地图的点云消息
     rclcpp::Publisher<octomap_msgs::msg::Octomap>::SharedPtr binary_map_pub_;
